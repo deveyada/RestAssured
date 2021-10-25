e46faf9735bf16ca8489b76ed85f49d4bfab7e70
deveyada <kumar.devesh04@gmail.com>
2021-10-25 16:45:11 +0530

    added request and response spec builders

--- src/Pojo/googleapi.java
+++ src/Pojo/googleapi.java
@@ -3,7 +3,10 @@ package Pojo;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.parsing.Parser;
+import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.*;
 
@@ -12,18 +15,27 @@ import java.util.List;
 
 public class googleapi {
 	
+	RequestSpecBuilder req = new RequestSpecBuilder();
+	ResponseSpecBuilder res = new ResponseSpecBuilder();
+	
 	@Test
 	
 	public void googlepostapi() {
 		
 		GApidetails gd = new GApidetails();
 		
+		req.setBaseUri("https://rahulshettyacademy.com");
+		req.setBasePath("/maps/api/place/add/json");
+		req.addQueryParam("key", "qaclick123");
+		
+		RequestSpecification rs = req.build();
+		
 		List<String> lst = new ArrayList<String>();
-		lst.add(0, "shoe parkere");
+		lst.add(0, "newly created");
 		lst.add(1, "shop");
 		Gapilocation a = new Gapilocation();
 		a.setLat(-67.889);
-		a.setLng(23.8787);
+		a.setLng(23.667);
 		
 		gd.setAccuracy(50);
 		gd.setAddress("29, side layout, cohen 09");
@@ -34,9 +46,11 @@ public class googleapi {
 		gd.setTypes(lst);
 		gd.setLocation(a);		
 		
-		RestAssured.baseURI = "https://rahulshettyacademy.com";
-		String Response = given().log().all().queryParam("key", "qaclick123").body(gd).expect().defaultParser(Parser.JSON)
-		.when().post("/maps/api/place/add/json").then().assertThat().statusCode(200).extract().response().asString();	
+		
+		String Response = given().log().all().spec(rs).body(gd).expect().defaultParser(Parser.JSON)
+		.when().post().then().extract().response().asString();
+		
+		res.expectStatusCode(200);
 		
 		System.out.println(Response);
 	}
